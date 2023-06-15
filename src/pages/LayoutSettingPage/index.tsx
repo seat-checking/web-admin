97c8bb33e3385866097ad6660199908fb0ef7fd3
Sumin Sohn <soyw31@gmail.com>
2023-06-15 23:36:29 +0900

    [design] 좌석설정 페이지 퍼블리싱 일부 (SideBar 사용 예시 작성)
    - <SideBar>가 들어가는 페이지 예시를 만들어놓기 위해 일부만 퍼블리싱 해놓음

--- src/pages/LayoutSettingPage/index.tsx
+++ src/pages/LayoutSettingPage/index.tsx
@@ -1,6 +1,22 @@
+import type { TabItem } from 'components/Tabs.tsx';
+import { SideBar } from 'components/SideBar';
+import { Tabs } from 'components/Tabs.tsx';
+import { SeatLayoutTab } from 'pages/LayoutSettingPage/components/SeatLayoutTab';
+import { ShopLayoutTab } from 'pages/LayoutSettingPage/components/ShopLayoutTab';
+
+const tabList: TabItem[] = [
+  { label: '가게 형태', content: <ShopLayoutTab /> },
+  { label: '좌석 배치', content: <SeatLayoutTab /> },
+];
 /**
- * 컴포넌트
+ * 좌석 설정 페이지
  */
 export const LayoutSettingPage: React.FC = () => {
-  return <div>LayoutSettingPage</div>;
+  return (
+    <div>
+      <SideBar style={{ paddingTop: '6rem' }}>
+        <Tabs tabList={tabList} />
+      </SideBar>
+    </div>
+  );
 };
