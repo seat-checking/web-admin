9d14c138f114f75350b4b19947029881f6f695c8
Sumin Sohn <soyw31@gmail.com>
2023-06-20 12:07:01 +0900

    [feat] Input에 ref 전달할 수 있도록 변경

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,5 @@
+import { forwardRef } from 'react';
+import type { Ref } from 'react';
 import {
   Label,
   LabelText,
@@ -6,30 +8,32 @@ import {
   RequiredAsterisk,
 } from 'components/Input.styled';
 
-interface InputProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
+interface InputProps
+  extends React.HtmlHTMLAttributes<HTMLDivElement & HTMLInputElement> {
   label: string;
   placeholder: string;
-  required?: boolean;
+  required?: boolean; // false면 * 표시 숨김
 }
 
 /**
  * 인풋 컴포넌트
  */
-export const Input: React.FC<InputProps> = ({
-  label,
-  placeholder,
-  required = true,
-  ...rest
-}) => {
-  return (
-    <Wrap {...rest}>
-      <Label>
-        <LabelText>
-          {label}
-          {required && <RequiredAsterisk>*</RequiredAsterisk>}
-        </LabelText>
-        <StyledInput placeholder={placeholder} />
-      </Label>
-    </Wrap>
-  );
-};
+export const Input = forwardRef(
+  (
+    { label, placeholder, required = true, ...rest }: InputProps,
+    ref: Ref<HTMLInputElement>,
+  ) => {
+    return (
+      // TODO rest 처리
+      <Wrap>
+        <Label>
+          <LabelText>
+            {label}
+            {required && <RequiredAsterisk>*</RequiredAsterisk>}
+          </LabelText>
+          <StyledInput placeholder={placeholder} ref={ref} {...rest} />
+        </Label>
+      </Wrap>
+    );
+  },
+);
