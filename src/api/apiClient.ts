ea5ce07d7e6129b14524bd0fa805d0b21afe393f
Sumin Sohn <soyw31@gmail.com>
2023-07-12 17:14:24 +0900

    [feat] 응답 인터셉터 추가, 네트워크 오류 발생 시 alert 띄움

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import type { AxiosResponse } from 'axios';
 
 const ENDPOINT = process.env.REACT_APP_API_URL;
 
@@ -9,3 +10,16 @@ export const axiosClient = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+axiosClient.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    console.log('error (응답 인터셉터) :>> ', error);
+    if (error.code === AxiosError.ERR_NETWORK) {
+      window.alert('네트워크 오류');
+    }
+    return Promise.reject(error);
+  },
+);
