eb655ea5a49eaeb8d9de25073e02eadd34816491
Sumin Sohn <soyw31@gmail.com>
2023-08-03 14:18:32 +0900

    [design] InputCheckBox.tsx 높이값 지정

--- src/components/InputCheckBox.styled.ts
+++ src/components/InputCheckBox.styled.ts
@@ -1,27 +1,30 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import checkBoxImg from 'assets/icons/checkbox.svg';
 import checkedBoxImg from 'assets/icons/checkedbox.svg';
 
 export const InputCheckBoxWrapper = styled.div`
   max-width: 67.5rem;
+  height: 2.4rem;
 `;
 
 export const Input = styled.input`
-appearance: none;
-border: 0.15rem solid gainsboro;
-border-radius: 3.2rem;
-width: 2.4rem;
-height: 2.4rem;
-cursor: pointer;
-background-image: url(${checkBoxImg});
-background-size: 100% 100%;
-background-position: 50%;
-background-repeat: no-repeat;
-
-&:checked {
-  border-color: transparent;
-  background-image: url(${checkedBoxImg});
+  appearance: none;
+  border: 0.15rem solid gainsboro;
+  border-radius: 3.2rem;
+  width: 2.4rem;
+  height: 2.4rem;
+  cursor: pointer;
+  background-image: url(${checkBoxImg});
   background-size: 100% 100%;
   background-position: 50%;
   background-repeat: no-repeat;
-  background-color: #ff8d4e;`;
+
+  &:checked {
+    border-color: transparent;
+    background-image: url(${checkedBoxImg});
+    background-size: 100% 100%;
+    background-position: 50%;
+    background-repeat: no-repeat;
+    background-color: #ff8d4e;
+  }
+`;
