ad46bdd48747b430b056cda5cd618dc6a0fa5ff0
Sumin Sohn <soyw31@gmail.com>
2023-08-10 17:22:43 +0900

    [design] 가게 현황 페이지 퍼블리싱

--- src/pages/ShopStatusPage/index.tsx
+++ src/pages/ShopStatusPage/index.tsx
@@ -1,6 +1,34 @@
+import { useTab } from 'common/hooks/useTab';
+import { Tabs } from 'components/Tabs.tsx';
+import { StyledSideBar } from 'pages/LayoutSettingPage/LayoutSettingPage.styled';
+import { ReservationTab } from 'pages/ShopStatusPage/ReservationTab';
+import { SeatTab } from 'pages/ShopStatusPage/SeatTab';
+import { Wrap } from 'pages/ShopStatusPage/ShopStatusPage.styled';
+
 /**
- * 컴포넌트
+ * 가게 현황 페이지
  */
 export const ShopStatusPage: React.FC = () => {
-  return <div>ShopStatusPage</div>;
+  const { activeTab, changeTab } = useTab();
+
+  return (
+    <Wrap>
+      <StyledSideBar>
+        <Tabs
+          onClickTab={changeTab}
+          activeTab={activeTab}
+          tabList={[
+            {
+              label: '예약 관리',
+              content: <ReservationTab />,
+            },
+            {
+              label: '좌석 정보',
+              content: <SeatTab />,
+            },
+          ]}
+        />
+      </StyledSideBar>
+    </Wrap>
+  );
 };
