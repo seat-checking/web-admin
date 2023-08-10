35b4d032a67e8d1b989602aee00c1c46ea1f0660
Sumin Sohn <soyw31@gmail.com>
2023-08-10 17:14:47 +0900

    [design] 가게 현황 > 예약 관리 탭 - 예약 상태 탭바 퍼블리싱

---
+++ src/pages/ShopStatusPage/ReservationTab/components/StatusTag.tsx
@@ -0,0 +1,45 @@
+import styled, { css } from 'styled-components';
+
+interface ReservationTagProps {
+  active: boolean;
+  children: React.ReactNode;
+  onClick?: VoidFunction;
+}
+
+/**
+ * 예약 상태 개별 탭 컴포넌트
+ */
+export const StatusTag: React.FC<ReservationTagProps> = ({
+  active,
+  children,
+  onClick,
+}) => {
+  return (
+    <Button type='button' onClick={onClick} $active={active}>
+      {children}
+    </Button>
+  );
+};
+
+const Button = styled.button<{ $active: boolean }>`
+  padding: 0.8rem 2rem;
+  border: 1px solid ${({ theme }) => theme.palette.grey[300]};
+  border-radius: 5.6rem;
+  color: ${({ theme }) => theme.palette.grey[500]};
+
+  &:hover {
+    ${({ $active }) =>
+      !$active &&
+      css`
+        background-color: rgba(0, 0, 0, 0.2);
+      `}
+  }
+
+  ${({ $active }) =>
+    $active &&
+    css`
+      border: 1px solid ${({ theme }) => theme.palette.grey[500]};
+      background-color: ${({ theme }) => theme.palette.grey[500]};
+      color: white;
+    `}
+`;
