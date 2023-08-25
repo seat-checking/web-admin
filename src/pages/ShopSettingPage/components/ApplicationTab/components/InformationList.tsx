c1f2157d15660425fd44e38248b2bf4a908aff9d
wooyoung6685 <wooyoung6685@gmail.com>
2023-08-25 11:21:05 +0900

    [add] 요청 정보 목록 api 연동

--- src/pages/ShopSettingPage/components/ApplicationTab/components/InformationList.tsx
+++ src/pages/ShopSettingPage/components/ApplicationTab/components/InformationList.tsx
@@ -1,113 +1,297 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useTheme } from 'styled-components';
+import type { SuccessOkResponse } from 'api/store/common';
+import type {
+  StoreCustomReservationField,
+  StoreCustomReservationResponse,
+} from 'api/store/store';
 import type React from 'react';
+import {
+  deleteRequestInformation,
+  getRequestInformation,
+  patchRequestInformation,
+} from 'api/store/store';
 
+import { STORAGE } from 'common/utils/constants';
 import { Button } from 'components/Button';
 import { Input } from 'components/Input';
+import { Modal } from 'components/Modal';
 import { Radio } from 'components/Radio';
+
 import {
-  ButtonWrapper,
   EditIcon,
   FlexWrapper,
   IconWrapper,
-  InputWrapper,
-  RadioWrapper,
   TitleInput,
-} from 'pages/ShopSettingPage/components/ApplicationTab/ApplicationTab.styled';
+} from 'pages/ShopSettingPage/components/ApplicationTab/components/InformationAdd.styled';
 import {
   ButtonWrap,
+  InputWrapper,
   ListContent,
   ListWrapper,
+  RadioWrapper,
   TitleText,
 } from 'pages/ShopSettingPage/components/ApplicationTab/components/InformationList.styled';
 import { SelectInput } from 'pages/ShopSettingPage/components/ApplicationTab/components/SelectInput';
-import { ToggleIcon } from 'pages/ShopSettingPage/components/EmployerTab/StaffListItem.styled';
+import {
+  ModaMainText,
+  ModaSubText,
+  ModalButton,
+  ModalButtonWrapper,
+  ModalCancel,
+  ModalContent,
+  ModalHeader,
+  ToggleIcon,
+} from 'pages/ShopSettingPage/components/EmployerTab/StaffListItem.styled';
 
-export const InformationList = () => {
+interface InformationListProps {
+  data: SuccessOkResponse<StoreCustomReservationResponse> | null;
+  fetchData: () => void; // fetchData의 실제 타입에 맞게 교체
+}
+
+export const InformationList: React.FC<InformationListProps> = ({
+  data,
+  fetchData,
+}) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [selectedItems, setSelectedItems] = useState<{ value: string }[]>([]); // 선택된 항목들의 상태
-  const [isOpen, setIsOpen] = useState(true); // 상태 추가
+  const [selectedItems, setSelectedItems] = useState<{ value: string }[]>([]);
   const [selectedRadio, setSelectedRadio] = useState('자유 입력');
+  const [modalOpen, setModalOpen] = useState<number | null>(null);
+  const [openedItems, setOpenedItems] = useState<{ [id: number]: boolean }>({});
+  const [editedItems, setEditedItems] = useState<{
+    [id: number]: { title?: string; contentGuide?: string };
+  }>({});
+  const [radioStates, setRadioStates] = useState<{ [id: number]: string }>({});
+
+  const theme = useTheme();
 
   const handleItemsChange = (items: { value: string }[]) => {
     setSelectedItems(items);
-    // 필요한 경우 여기에 추가적인 로직을 작성할 수 있습니다.
   };
 
-  const toggleIsOpen = () => {
-    // 함수 추가
-    setIsOpen(!isOpen);
+  const toggleItemOpen = (id: number) => {
+    setOpenedItems((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
   };
 
-  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedRadio(event.target.value); // 라디오 버튼 값 저장
+  const handleRadioChange = (id: number, value: string) => {
+    setRadioStates((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
   };
+  const openModal = (id: number) => {
+    setModalOpen(id);
+  };
+
+  const closeModal = () => {
+    setModalOpen(null);
+  };
+
+  useEffect(() => {
+    if (data) {
+      const initialRadioStates =
+        data.result.storeCustomReservationFieldList.reduce<{
+          [key: number]: string;
+        }>((acc, item) => {
+          acc[item.id] = item.type;
+          return acc;
+        }, {});
+      setRadioStates(initialRadioStates);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (data && data.result.storeCustomReservationFieldList.length > 0) {
+      const fieldType = data.result.storeCustomReservationFieldList[0].type;
+      setSelectedRadio(fieldType);
+    }
+  }, [data]);
+
+  if (!data) return null;
+
+  const handleDelete = async (customid: number) => {
+    if (modalOpen !== null) {
+      try {
+        const storeId = localStorage.getItem(STORAGE.storeId);
+        if (!storeId) {
+          console.error('Store ID is missing.');
+          return;
+        }
+
+        const Params = {
+          storeId,
+          customid,
+        };
+
+        await deleteRequestInformation(Params);
+        fetchData();
+        closeModal();
+      } catch (error) {
+        console.error('error');
+      }
+    }
+  };
+
+  const handleSave = async (item: StoreCustomReservationField) => {
+    const storeId = localStorage.getItem(STORAGE.storeId);
+    if (!storeId) {
+      console.error('Store ID is missing.');
+      return;
+    }
+
+    const editedTitle = editedItems[item.id]?.title || item.title;
+
+    let editedContentGuide: string[];
+
+    if (item.type === '자유 입력') {
+      editedContentGuide = [
+        editedItems[item.id]?.contentGuide || item.contentGuide,
+      ];
+    } else {
+      editedContentGuide = selectedItems.map((i) => i.value);
+    }
+
+    const resData = {
+      title: editedTitle,
+      type: radioStates[item.id],
+      contentGuide: editedContentGuide,
+    };
+    const Params = {
+      storeId,
+      data: resData,
+      customid: item.id,
+    };
+
+    try {
+      await patchRequestInformation(Params);
+      fetchData();
+      console.log('Data saved successfully');
+    } catch (error) {
+      console.error('Error saving data:', error);
+    }
+  };
+
   return (
-    <ListWrapper>
-      <ListContent>
-        <FlexWrapper>
-          <IconWrapper>
-            {isOpen ? (
-              <>
-                <TitleInput
-                  placeholder='*제목을 입력해주세요'
-                  onFocus={() => setIsEditing(true)}
-                  onBlur={() => setIsEditing(false)}
-                  focused={isEditing}
+    <>
+      {data.result.storeCustomReservationFieldList.map((item) => (
+        <ListWrapper key={item.id}>
+          <ListContent>
+            <FlexWrapper>
+              <IconWrapper>
+                {openedItems[item.id] ? (
+                  <>
+                    <TitleInput
+                      style={{ background: '#fff' }}
+                      value={
+                        typeof editedItems[item.id]?.title !== 'undefined'
+                          ? editedItems[item.id]?.title
+                          : item.title
+                      }
+                      placeholder='*제목을 입력해주세요'
+                      onFocus={() => setIsEditing(true)}
+                      onBlur={() => setIsEditing(false)}
+                      focused={isEditing}
+                      onChange={(e) => {
+                        const newValue = e.target.value;
+                        setEditedItems((prev) => ({
+                          ...prev,
+                          [item.id]: { ...prev[item.id], title: newValue },
+                        }));
+                      }}
+                    />
+                    {!isEditing && <EditIcon />}
+                  </>
+                ) : (
+                  <TitleText>{item.title}</TitleText>
+                )}
+              </IconWrapper>
+              <RadioWrapper>
+                <Radio
+                  label='자유 입력'
+                  value='자유 입력'
+                  size='small'
+                  id={`option1-${item.title}`}
+                  onChange={() => handleRadioChange(item.id, '자유 입력')}
+                  checked={item.type === '자유 입력'}
                 />
-                {!isEditing && <EditIcon />}
-              </>
-            ) : (
-              <TitleText>여기에 일반 텍스트를 입력하세요.</TitleText>
-            )}
-          </IconWrapper>
-          <RadioWrapper>
-            <Radio
-              label='자유 입력'
-              name='라디오버튼'
-              value='자유 입력'
-              size='small'
-              id='option11'
-              onChange={handleRadioChange}
-            />
-            <Radio
-              label='선택지 제공'
-              name='라디오버튼'
-              value='선택지 제공'
-              size='small'
-              id='option22'
-              onChange={handleRadioChange}
-            />
-          </RadioWrapper>
-          <ToggleIcon onClick={toggleIsOpen} isOpen={isOpen} />
-        </FlexWrapper>
-        {isOpen && (
-          <InputWrapper>
-            {selectedRadio === '자유 입력' && (
-              <Input placeholder='고객에게 가이드를 작성해주세요.(ex: 사용 목적을 입력해주세요)' />
-            )}
-            {selectedRadio === '선택지 제공' && (
-              <SelectInput
-                type='text'
-                placeholder='고객에게 가이드를 작성해주세요.(ex: 사용 목적을 입력해주세요)'
-                onItemsChange={handleItemsChange}
-                isActive={false}
+                <Radio
+                  label='선택지 제공'
+                  value='선택지 제공'
+                  size='small'
+                  id={`option2-${item.title}`}
+                  onChange={() => handleRadioChange(item.id, '선택지 제공')}
+                  checked={item.type === '선택지 제공'}
+                />
+              </RadioWrapper>
+              <ToggleIcon
+                onClick={() => toggleItemOpen(item.id)}
+                isOpen={openedItems[item.id]}
               />
+            </FlexWrapper>
+            {openedItems[item.id] && (
+              <InputWrapper>
+                {item.type === '자유 입력' && (
+                  <Input
+                    defaultValue={JSON.parse(item.contentGuide)}
+                    placeholder='고객에게 가이드를 작성해주세요.(ex: 사용 목적을 입력해주세요)'
+                    onChange={(e) => {
+                      const newValue = e.target.value;
+                      setEditedItems((prev) => ({
+                        ...prev,
+                        [item.id]: { ...prev[item.id], contentGuide: newValue },
+                      }));
+                    }}
+                  />
+                )}
+                {item.type === '선택지 제공' && (
+                  <SelectInput
+                    defaultValue={JSON.parse(item.contentGuide)}
+                    type='text'
+                    placeholder='고객에게 가이드를 작성해주세요.(ex: 사용 목적을 입력해주세요)'
+                    isActive={false}
+                    onItemsChange={handleItemsChange}
+                  />
+                )}
+                <ButtonWrap>
+                  <Button
+                    style={{
+                      width: '26.5rem',
+                      color: theme.palette.grey[400],
+                      background: theme.palette.grey[100],
+                      fontSize: '1.4rem',
+                    }}
+                    onClick={() => openModal(item.id)}
+                  >
+                    삭제하기
+                  </Button>
+                  <Button
+                    style={{ width: '26.5rem', fontSize: '1.4rem' }}
+                    onClick={() => handleSave(item)}
+                  >
+                    저장하기
+                  </Button>
+                </ButtonWrap>
+              </InputWrapper>
             )}
-            <ButtonWrap>
-              <Button
-                style={{
-                  width: '26.5rem',
-                  color: '#727582',
-                  background: '#EFF0F5',
-                }}
-              >
-                삭제하기
-              </Button>
-              <Button style={{ width: '26.5rem' }}>저장하기</Button>
-            </ButtonWrap>
-          </InputWrapper>
-        )}
-      </ListContent>
-    </ListWrapper>
+          </ListContent>
+          <Modal isOpen={modalOpen === item.id} onClose={closeModal}>
+            <ModalHeader>정보 목록 삭제</ModalHeader>
+            <ModalContent>
+              <ModaMainText>정말 목록을 삭제하시나요?</ModaMainText>
+              <ModaSubText>삭제한 목록은 복구할 수 없어요!</ModaSubText>
+            </ModalContent>
+            <ModalButtonWrapper>
+              <ModalCancel onClick={closeModal}>취소</ModalCancel>
+              <ModalButton onClick={() => handleDelete(item.id)}>
+                삭제
+              </ModalButton>
+            </ModalButtonWrapper>
+          </Modal>
+        </ListWrapper>
+      ))}
+    </>
   );
 };
