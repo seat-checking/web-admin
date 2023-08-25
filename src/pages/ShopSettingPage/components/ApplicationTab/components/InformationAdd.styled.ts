78bf77a77a0e4cca71094c49762165b8e0be46f6
wooyoung6685 <wooyoung6685@gmail.com>
2023-08-25 11:20:14 +0900

    [add] 요청 정보 추가 컴포넌트 생성

---
+++ src/pages/ShopSettingPage/components/ApplicationTab/components/InformationAdd.styled.ts
@@ -0,0 +1,115 @@
+import styled from 'styled-components';
+import Edit from 'assets/icons/edit-3.svg';
+import PlusCircl from 'assets/icons/plus.svg';
+
+interface TitleInputProps {
+  focused: boolean;
+}
+
+export const LabelWrapper = styled.div`
+  width: 53.9rem;
+  margin: 0 auto;
+  padding-top: 4rem;
+`;
+
+export const ApplicationTabWrapper = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  background-color: ${({ theme }) => theme.palette.grey[50]};
+`;
+
+export const InputWrapper = styled.div`
+  width: 53.9rem;
+  margin: 0 auto;
+  height: auto;
+`;
+
+export const Border = styled.div`
+  width: 100%;
+  height: 4rem;
+  border-bottom: 1px solid ${({ theme }) => theme.palette.grey[100]};
+`;
+
+export const TitleInput = styled.input<TitleInputProps>`
+  width: 14.8rem;
+  height: 2.4rem;
+  color: ${({ theme }) => theme.palette.black.main};
+  font-size: 1.6rem;
+  font-weight: 600;
+  border: ${({ focused }) => (focused ? '0.05rem solid black' : 'none')};
+  border-radius: 0.4rem;
+  transition: border 0.3s ease;
+  margin-right: ${({ focused }) => (focused ? '1.6rem;' : 'none')};
+  background-color: ${({ focused }) => (focused ? '#fff' : '#F2F4F8')};
+
+  ::placeholder {
+    color: ${({ theme }) => theme.palette.grey[300]};
+    font-size: 1.6rem;
+    font-weight: 600;
+  }
+`;
+
+export const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const EditIcon = styled.div`
+  width: 1.6rem;
+  height: 1.6rem;
+  background-image: url(${Edit});
+  background-size: cover;
+  cursor: pointer;
+`;
+
+export const DeleteButton = styled.button`
+  width: 2.5rem;
+  height: 2.4rem;
+  color: ${({ theme }) => theme.palette.grey[500]};
+  font-size: 1.4rem;
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+export const RadioWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4rem;
+`;
+export const FlexWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 4.4rem;
+  padding-top: 4.4rem;
+  width: 53.9rem;
+`;
+
+export const PlusCirclIcon = styled.div`
+  width: 3.2rem;
+  height: 3.2rem;
+  background-image: url(${PlusCircl});
+  background-size: cover;
+`;
+
+export const PlusText = styled.div`
+  color: ${({ theme }) => theme.palette.grey[400]};
+  font-size: 1.8rem;
+  font-weight: 600;
+`;
+
+export const PlusWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+  cursor: pointer;
+  margin: 0 auto;
+  width: 10.3rem;
+  margin-top: 3.2rem;
+`;
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.8rem;
+`;
---
+++ src/pages/ShopSettingPage/components/ApplicationTab/components/InformationAdd.tsx
@@ -0,0 +1,206 @@
+import { useState } from 'react';
+import type React from 'react';
+import { requestInformation } from 'api/store/store';
+import { STORAGE } from 'common/utils/constants';
+import { Button } from 'components/Button';
+import { Input } from 'components/Input';
+import { Label } from 'components/Label';
+import { Radio } from 'components/Radio';
+
+import {
+  ApplicationTabWrapper,
+  ButtonWrapper,
+  EditIcon,
+  FlexWrapper,
+  IconWrapper,
+  InputWrapper,
+  RadioWrapper,
+  TitleInput,
+} from 'pages/ShopSettingPage/components/ApplicationTab/components/InformationAdd.styled';
+import { SelectInput } from 'pages/ShopSettingPage/components/ApplicationTab/components/SelectInput';
+import { LabelWrapper } from 'pages/ShopSettingPage/components/EmployerTab/EmployerTab.styled';
+
+interface InputFieldProps {
+  id: number;
+  option: string;
+  title: string;
+  type: string;
+  contentGuide: string;
+}
+
+interface InformationAddProps {
+  fetchData: () => void;
+}
+
+export const InformationAdd: React.FC<InformationAddProps> = ({
+  fetchData,
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [inputFields, setInputFields] = useState<InputFieldProps[]>([
+    {
+      id: Date.now(),
+      option: 'option1',
+      title: '',
+      type: '자유 입력',
+      contentGuide: '',
+    },
+  ]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleRadioChange = (fieldId: number, value: string) => {
+    const selectedType = value === 'option1' ? '자유 입력' : '선택지 제공';
+
+    setInputFields(
+      inputFields.map((field) =>
+        field.id === fieldId
+          ? { ...field, option: value, type: selectedType }
+          : field,
+      ),
+    );
+  };
+
+  const handleItemsChange = (fieldId: number, items: { value: string }[]) => {
+    setInputFields(
+      inputFields.map((field) =>
+        field.id === fieldId
+          ? {
+              ...field,
+              contentGuide: items.map((item) => item.value).join(','),
+            } // 모든 아이템의 value를 쉼표로 구분된 문자열로 저장
+          : field,
+      ),
+    );
+  };
+
+  const handleTitleChange =
+    (fieldId: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputFields(
+        inputFields.map((field) =>
+          field.id === fieldId ? { ...field, title: e.target.value } : field,
+        ),
+      );
+    };
+
+  const handleContentGuideChange =
+    (fieldId: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputFields(
+        inputFields.map((field) =>
+          field.id === fieldId
+            ? { ...field, contentGuide: e.target.value }
+            : field,
+        ),
+      );
+    };
+
+  const handleSubmit = async () => {
+    const storeId = localStorage.getItem(STORAGE.storeId);
+    if (!storeId) {
+      console.error('error');
+      return;
+    }
+
+    setIsLoading(true);
+
+    const formattedData = inputFields.map((field) => ({
+      title: field.title,
+      type: field.type,
+      contentGuide: field.contentGuide.split(','),
+    }));
+
+    try {
+      const response = await requestInformation({
+        storeId,
+        data: formattedData[0],
+      });
+      fetchData();
+      setIsLoading(false);
+      setInputFields([
+        {
+          id: Date.now(),
+          option: 'option1',
+          title: '',
+          type: '자유 입력',
+          contentGuide: '',
+        },
+      ]);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <>
+      <ApplicationTabWrapper>
+        <LabelWrapper>
+          <Label label='고객에게 받을 정보' />
+        </LabelWrapper>
+        {inputFields.map((field) => (
+          <InputWrapper key={field.id}>
+            <FlexWrapper>
+              <IconWrapper>
+                <TitleInput
+                  placeholder='*제목을 입력해주세요'
+                  onFocus={() => setIsEditing(true)}
+                  onBlur={() => setIsEditing(false)}
+                  focused={isEditing}
+                  value={field.title}
+                  onChange={handleTitleChange(field.id)}
+                />
+                {!isEditing && <EditIcon />}
+              </IconWrapper>
+              <RadioWrapper>
+                <Radio
+                  label='자유 입력'
+                  id='option1'
+                  name={field.id.toString()}
+                  value={field.type}
+                  size='small'
+                  onChange={() => handleRadioChange(field.id, 'option1')}
+                  defaultChecked
+                />
+                <Radio
+                  label='선택지 제공'
+                  id='option2'
+                  name={field.id.toString()}
+                  value={field.type}
+                  size='small'
+                  onChange={() => handleRadioChange(field.id, 'option2')}
+                />
+              </RadioWrapper>
+            </FlexWrapper>
+            {field.option === 'option1' && (
+              <Input
+                placeholder='고객에게 가이드를 작성해주세요.(ex: 사용 목적을 입력해주세요)'
+                value={field.contentGuide}
+                onChange={handleContentGuideChange(field.id)}
+              />
+            )}
+            {field.option === 'option2' && (
+              <SelectInput
+                placeholder='고객에게 가이드를 작성해주세요.(ex: 사용 목적을 입력해주세요)'
+                type='text'
+                onItemsChange={(items) => handleItemsChange(field.id, items)}
+                isActive
+              />
+            )}
+          </InputWrapper>
+        ))}
+        <ButtonWrapper>
+          <Button
+            style={{
+              marginTop: '2.4rem',
+              width: '53.9rem',
+              marginBottom: '4rem',
+            }}
+            onClick={handleSubmit}
+          >
+            요청 추가하기
+          </Button>
+        </ButtonWrapper>
+      </ApplicationTabWrapper>
+      <LabelWrapper>
+        <Label required={false} label='요청 정보 목록' />
+      </LabelWrapper>
+    </>
+  );
+};
