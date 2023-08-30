c9f2fc4b2c77ede6ff53e144212addcb5c18880a
wooyoung6685 <wooyoung6685@gmail.com>
2023-08-30 15:13:21 +0900

    [add] 타임셀렉트 프롭스 추가

--- src/pages/ShopSettingPage/components/BusinessHourTab/components/TimeSelect.tsx
+++ src/pages/ShopSettingPage/components/BusinessHourTab/components/TimeSelect.tsx
@@ -5,10 +5,11 @@ import type { Dayjs } from 'dayjs';
 import { ReactComponent as ChevronDown } from 'assets/icons/chevron-down.svg';
 
 interface TimeSelectProps {
-  defaultValue?: string; // '23:30' 형태
+  defaultValue?: string | null; // '23:30' 형태
   onChange?: (event: string) => void;
   onBlur?: (event: string) => void;
   selected?: string; //
+  disabled?: boolean;
 }
 
 /**
@@ -19,6 +20,7 @@ export const TimeSelect: React.FC<TimeSelectProps> = ({
   onChange: propOnChange,
   onBlur: propOnBlur,
   selected,
+  disabled,
 }) => {
   const theme = useTheme();
   const internalOnChange = (time: Dayjs | null, timeString: string) => {
@@ -31,16 +33,18 @@ export const TimeSelect: React.FC<TimeSelectProps> = ({
       use12Hours={false}
       format='HH:mm'
       onChange={internalOnChange}
-      defaultValue={defaultValue ? dayjs(defaultValue, 'hh:mm') : undefined}
+      defaultValue={defaultValue ? dayjs(defaultValue, 'HH:mm') : undefined}
+      value={selected ? dayjs(selected, 'HH:mm') : undefined}
       allowClear={false}
       minuteStep={30}
       showNow={false}
+      disabled={disabled}
       suffixIcon={<ChevronDown stroke={theme.palette.grey[300]} />}
       size='small'
       placeholder='지정되지 않음'
       style={{
         flex: 1,
-        height: '4.8rem',
+        height: '42px',
         fontSize: '1.8rem',
         padding: '1.2rem',
       }}
