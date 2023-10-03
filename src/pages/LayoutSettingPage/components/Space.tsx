0fecc9e31d022e8e3c4fd3cb6644e4df646921ee
Sumin Sohn <soyw31@gmail.com>
2023-10-03 12:16:52 +0900

    [refact] Space 컴포넌트에서 스타일 분리
    가게 현황 페이지에서도 Space의 스타일을 써야되기 때문에 분리함

--- src/pages/LayoutSettingPage/components/Space.tsx
+++ src/pages/LayoutSettingPage/components/Space.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
-import styled, { css, useTheme } from 'styled-components/macro';
+import { useTheme } from 'styled-components/macro';
 import { ReactComponent as EditIcon } from 'assets/icons/edit-md.svg';
 import { ReactComponent as XIcon } from 'assets/icons/x.svg';
 
 import { IconButton } from 'components/XButton';
 import { DeleteSpaceModal } from 'pages/LayoutSettingPage/components/DeleteSpaceModal';
+import {
+  BtnsRow,
+  Name,
+  SpaceBox,
+} from 'pages/LayoutSettingPage/components/Space.styled';
 import { SpaceAddEditModal } from 'pages/LayoutSettingPage/components/SpaceAddEditModal';
 import { useSpaceId } from 'pages/LayoutSettingPage/hooks/useSpaceId';
-import { flexSet } from 'styles/mixin';
 
 interface SpaceProps {
   id: number;
@@ -73,51 +77,3 @@ export const Space: React.FC<SpaceProps> = ({
     </SpaceBox>
   );
 };
-
-export const SpaceBox = styled.div<{ isSelected: boolean }>`
-  position: relative;
-  cursor: pointer;
-
-  width: 17.1rem;
-  height: 9.1rem;
-  border-radius: 0.8rem;
-
-  background-color: #fff;
-  opacity: 0.5;
-
-  & + & {
-    margin-left: 1.2rem;
-  }
-
-  ${flexSet()}
-
-  input {
-  }
-
-  ${({ isSelected }) =>
-    isSelected &&
-    css`
-      opacity: 1;
-    `}
-`;
-
-export const Name = styled.div`
-  max-width: 16rem;
-
-  text-align: center;
-  font-weight: 500;
-  font-size: 2.4rem;
-
-  /* background-color: aqua; */
-`;
-
-export const BtnsRow = styled.div`
-  position: absolute;
-  top: 0.4rem;
-  right: 0.4rem;
-
-  display: flex;
-  gap: 1rem;
-
-  /* background-color: yellow; */
-`;
