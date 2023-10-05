a141841c5d912b0028b0be78b1bac39ddd70d068
Sumin Sohn <soyw31@gmail.com>
2023-10-05 19:43:22 +0900

    [feat] 리액트쿼리 마운트, 윈도우 포커싱때 refetch 하지 않도록함

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { CustomToastContainer } from 'components/CustomToastContainer';
+import { LoadingSpinner } from 'components/LoadingSpinner';
 import { router } from 'pages/Router';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      retry: 2,
+    },
+  },
+});
 
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
+      <RouterProvider router={router} fallbackElement={<LoadingSpinner />} />
       <CustomToastContainer />
     </QueryClientProvider>
   );
