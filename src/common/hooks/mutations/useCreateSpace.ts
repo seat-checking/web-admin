322ca99011256584e98dab89749d7e320cc607c4
Sumin Sohn <soyw31@gmail.com>
2023-10-11 02:30:30 +0900

    [fix] 스페이스 수정, 생성 완료 후 스페이스 목록 api 재호출
    이전엔 수정, 생성 전에 api 호출하고 있어서 변경되지 않은 목록을 다시 뿌려주고 있었음

--- src/common/hooks/mutations/useCreateSpace.ts
+++ src/common/hooks/mutations/useCreateSpace.ts
@@ -1,10 +1,13 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { ShopLayout } from 'api/shop/types';
 import { ShopApi } from 'api/shop';
+import { queryKeys } from 'common/utils/constants';
 import { useSpaceId } from 'pages/LayoutSettingPage/hooks/useSpaceId';
 import { useChange } from 'pages/LayoutSettingPage/stores/changeStore';
 
 export const useCreateSpace = () => {
+  const queryClient = useQueryClient();
+
   const { setSpaceId } = useSpaceId();
   const { setChange } = useChange();
 
@@ -18,7 +21,10 @@ export const useCreateSpace = () => {
     }) => {
       return ShopApi.createShopLayout(shopId, layout);
     },
-    onSuccess(createdSpaceId) {
+    onSuccess(createdSpaceId, { shopId }) {
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.GET_SPACES, shopId],
+      });
       setSpaceId(createdSpaceId);
       setChange(false);
     },
